9780e2069161625fcbbca6a254f65eb91c76463e
Anshumankumar <anshu266man@gmail.com>
2015-04-29 05:48:37 +0530

    Added the prerun and postrun

--- src/main.py
+++ src/main.py
@@ -17,25 +17,28 @@ def play():
     screen = pygame.display.set_mode(size)
     myfont = pygame.font.SysFont("monospace", 30)
     keys = pygame.key.get_pressed()
+    
+    #Pre Run
+    blinkflag = 0
     while (not keys[pygame.K_SPACE]):
+        blinkflag = blinkflag + 1
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
         textmain = myfont.render(
                "SPACE INVADER" , 1, (0,255,0))
         starttext = myfont.render(
                "Press space to start" , 1, (0,255,0))
-        screen.blit(starttext,(200,200))
+        if (blinkflag < 20):
+            screen.blit(starttext,(200,200))
+        if (blinkflag > 30):
+            blinkflag = 0
         screen.blit(textmain,(210,50))
         pygame.display.flip()
-        clock.tick(5)
-        screen.fill(black)
-        screen.blit(textmain,(210,50))
-
-        pygame.display.flip()
-        clock.tick(5)
+        clock.tick(30)
         screen.fill(black)
         keys = pygame.key.get_pressed()
 
+    #Main Run
     playerShip = PlayerShip([320,350])
     controller = Controller(14,6,screen)
     while not controller.gameover():
@@ -50,11 +53,31 @@ def play():
         controller.blit()
         controller.collision(playerShip.bullet)
         controller.player_collision_check(playerShip)
-        #TODO: Get a better way to do this
-        # Update objects on screen
         pygame.display.flip()
         clock.tick(30)
-    return
+    
+    # Post run
+    blinkflag = 0
+    while (not keys[pygame.K_SPACE]):
+        blinkflag = blinkflag + 1
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+        textmain = myfont.render(
+               "SPACE INVADER" , 1, (0,255,0))
+        starttext = myfont.render(
+               "GAME OVER" , 1, (0,255,0))
+        if (blinkflag < 20):
+            screen.blit(starttext,(230,200))
+        if (blinkflag > 30):
+            blinkflag = 0
+        screen.blit(textmain,(210,50))
+        pygame.display.flip()
+        clock.tick(30)
+        screen.fill(black)
+        keys = pygame.key.get_pressed()
+
+
 
 if __name__ == '__main__':
-    play()
+    while True:
+        play()
